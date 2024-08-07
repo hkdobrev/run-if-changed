b31f32735cc8f98c0894331433d4f8ab042ceb9a
Haralan Dobrev <harry@hkdobrev.com>
2024-08-07 15:49:29 +0300

    Replace findBinary with latest execa

--- src/runCommands.js
+++ src/runCommands.js
@@ -1,17 +1,16 @@
-import execa from 'execa';
-import findBinary from './findBinary.js';
+import { execaCommandSync } from 'execa';
 
-function runBinary(command) {
-  const { binaryPath, args } = command;
-  const result = execa(binaryPath, args);
-  result.stdout.pipe(process.stdout);
-  result.stderr.pipe(process.stderr);
-}
-
-function normaliseCommands(commands) {
-  return Array.isArray(commands) ? commands : [commands].filter((x) => !!x);
-}
+const runCommand = (command) => {
+  process.stdout.write(`${command}: \n`);
+  execaCommandSync({
+    preferLocal: true,
+    stdout: 'inherit',
+    stderr: 'inherit',
+  })(command);
+};
 
 export default (commands) => {
-  normaliseCommands(commands).map(findBinary).forEach(runBinary);
+  (Array.isArray(commands) ? commands : [commands])
+    .filter((x) => !!x)
+    .forEach(runCommand);
 };
