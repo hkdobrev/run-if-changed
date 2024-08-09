907b2e98875481880dbf8e2b4593ae45f7b62dd4
Haralan Dobrev <harry@hkdobrev.com>
2024-08-10 01:19:10 +0300

    Simplify file matcher making it more efficient

--- src/matcher.js
+++
@@ -1,3 +0,0 @@
-import micromatch from 'micromatch';
-
-export default (changedFiles, patterns) => micromatch.some(changedFiles, patterns);
--- src/resolveMatchingPatterns.js
+++ src/resolveMatchingPatterns.js
@@ -1,11 +1,9 @@
-import matcher from './matcher.js';
+import micromatch from 'micromatch';
 
-export default (list, config) => {
+export default (changedFiles, config) => {
   const commandsToRun = Object.entries(config)
-    .filter(([pattern]) => matcher(list, [pattern]))
-    .map(([, commands]) => commands)
-    // Flatten array
-    .reduce((acc, val) => acc.concat(val), []);
+    .filter(([pattern]) => micromatch.some(changedFiles, [pattern]))
+    .flatMap(([, commands]) => commands);
 
   // unique commands, do not run them multiple times if they are the same
   // for multiple patterns or they are repeated in a list for a single pattern
