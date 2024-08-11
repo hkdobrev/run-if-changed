478cbde312583044aac5c801ff38ebe2bec0349e
Haralan Dobrev <harry@hkdobrev.com>
2024-08-11 09:14:42 +0300

    Upgrade ESlint config from deprecated one to flat config

---
+++ eslint.config.js
@@ -0,0 +1,16 @@
+import js from '@eslint/js';
+import globals from 'globals';
+
+export default [
+  js.configs.recommended,
+  {
+    languageOptions: {
+      globals: {
+        ...globals.node,
+      },
+      parserOptions: {
+        ecmaVersion: 'latest',
+      },
+    },
+  },
+];
